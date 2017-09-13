507360c8adc5c1c9484429dfc305cd52161856f9
Bruno Lins <brunolins@simplificafretes.com.br>
2017-09-13 10:29:19 -0300

    add colors into project

--- AutoMailer.js
+++ AutoMailer.js
@@ -1,46 +1,40 @@
 var http = require('http');
 var cron = require('node-cron');
 var nodemailer = require('nodemailer');
-
+var colors = require('colors');
 // Criando Servidor
-var server = http.createServer(function(request, response){
-	response.writeHead(200,{"Content-Type": "text/html"});
+var server = http.createServer(function (request, response) {
+	response.writeHead(200, {
+		"Content-Type": "text/html"
+	});
 
-	cron.schedule('36 15 * * *', function NodeCron(){
-		
+	cron.schedule('00 00 * * *', function NodeCron() {
 		// Inicio do Mailer
-		
 		// Enviando e-mails usando o Node.js e o famoso nodemailer
-		var nodemailer = require('nodemailer');
-
 		// Vamos criar a conta que irá mandar os e-mails
 		var conta = nodemailer.createTransport({
 			service: 'Gmail',
 			auth: {
-        			user: 'blins44@gmail.com', // Seu usuário no Gmail
-        			pass: 'nike2011' // A senha da sua conta no Gmail
-        		}
-        	});
-
+				user: 'example@gmail.com', // Seu usuário no Gmail
+				pass: '*****' // A senha da sua conta no Gmail
+			}
+		});
 		conta.sendMail({
-    		from: 'Nome Empresa <blins44@gmail.com>', // Quem está mandando
-    		to: 'Eduardo Labre <eduardo@simplificafretes.com.br>, Luiz Paulo <luizpaulo@simplificafretes.com.br>', // Para quem o e-mail deve chegar
-    		subject: 'Fazendo testes de AutoMailer v1.0', // O assunto
-    		html: '<strong>Oi!</strong><p>Estou testando o AutoMailer! E esse é o corpo em HTML5 do meu e-mail.</p>', // O HTMl do nosso e-mail
-    	}, function(err){
-    		if(err)
-    			throw err;
-
-    		console.log('E-mail enviado com sucesso.');
-    	});
+			from: 'Nome Empresa <blins44@gmail.com>', // Quem está mandando
+			to: 'Bruno Lins <blins44@gmail.com>', // Para quem o e-mail deve chegar
+			subject: 'Fazendo testes de AutoMailer v1.0', // O assunto
+			html: '<strong>Oi!</strong><p>Estou testando o seu AutoMailer! @devBrunoLins</p>', // O HTMl do nosso e-mail
+		}, (err) => {
+			if (err) throw err;
+			console.log('E-mail enviado com sucesso.');
+		});
 		// Fim do Mailer
 	});
 	response.end();
 });
-
 // Listando o Servidor na Porta 3000
-server.listen(3000, function(){
-	var version = '1.0';
-	console.log('Acesse: localhost:3000 em seu navegador para iníciar o AutoMailer '+ version);
+server.listen(3000, function () {
+	const version = 'v1.0';
+	console.log(colors.green('Acesse: localhost:3000 em seu navegador para iníciar o AutoMailer ' + version));
+	console.log(colors.magenta('Ajude-nos também no aperfeiçoamento de nosso AutoMailer'));
 });
-
