a8e3808b37a54a8805be8383086265e941a505b2
Bruo Lins <bruno.lins.da.silva@avanade.com>
2018-03-28 22:28:22 -0300

    alterando data

--- AutoMailer.js
+++ AutoMailer.js
@@ -8,22 +8,22 @@ var server = http.createServer(function (request, response) {
 		"Content-Type": "text/html"
 	});
 
-	cron.schedule('00 00 * * *', function NodeCron() {
+	cron.schedule('23 22 * * *', function NodeCron() {
 		// Inicio do Mailer
 		// Enviando e-mails usando o Node.js e o famoso nodemailer
 		// Vamos criar a conta que irá mandar os e-mails
 		var conta = nodemailer.createTransport({
 			service: 'Gmail',
 			auth: {
-				user: 'example@gmail.com', // Seu usuário no Gmail
-				pass: '*****' // A senha da sua conta no Gmail
+				user: 'blins44@gmail.com', // Seu usuário no Gmail
+				pass: 'nike2011' // A senha da sua conta no Gmail
 			}
 		});
 		conta.sendMail({
-			from: 'Nome Empresa <blins44@gmail.com>', // Quem está mandando
-			to: 'Bruno Lins <blins44@gmail.com>', // Para quem o e-mail deve chegar
-			subject: 'Fazendo testes de AutoMailer v1.0', // O assunto
-			html: '<strong>Oi!</strong><p>Estou testando o seu AutoMailer! @devBrunoLins</p>', // O HTMl do nosso e-mail
+			from: 'Bruno Lins <blins44@gmail.com>', // Quem está mandando
+			to: 'Luiz Felipe Nicolini - Futuro Dev Avanade <luizfelipenicolini@gmail.com>', // Para quem o e-mail deve chegar
+			subject: 'Você está contratado! Parabens', // O assunto
+			html: '<strong>Oi!</strong><p>Estou testando o meu AutoMailer! @devBrunoLins</p>', // O HTMl do nosso e-mail
 		}, (err) => {
 			if (err) throw err;
 			console.log('E-mail enviado com sucesso.');
@@ -33,7 +33,7 @@ var server = http.createServer(function (request, response) {
 	response.end();
 });
 // Listando o Servidor na Porta 3000
-server.listen(3000, function () {
+server.listen(3000, () => {
 	const version = 'v1.0';
 	console.log(colors.green('Acesse: localhost:3000 em seu navegador para iníciar o AutoMailer ' + version));
 	console.log(colors.magenta('Ajude-nos também no aperfeiçoamento de nosso AutoMailer'));
